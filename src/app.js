9ad8283998222f325e36a469ed72a7989a0a0777
alana-as <abs_alana@hotmail.com>
2022-10-23 11:31:54 -0300

    Criação da porta e das rotas get

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const CalculadoraController = require("./controller/CalculadoraController");
+const app = express();
+
+app.use(express.json());
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+    console.log("Testando API Calculadora");
+    res.send("API Calculadora Todo-7");
+})
+
+app.get("/soma", CalculadoraController.soma);
+app.get("/subtracao", CalculadoraController.subtracao);
+app.get("/multiplicacao", CalculadoraController.multiplicacao);
+app.get("/divisao", CalculadoraCSontroller.divisao);
+
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
